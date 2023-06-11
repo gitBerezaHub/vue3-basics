955912d2354e91771ccfd2d4ed4777b565ac5009
Kirill <berezinkb06@gmail.com>
2023-06-11 22:26:26 +0800

    list of rand posts with deleting and adding features

---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "MyDialog"
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
